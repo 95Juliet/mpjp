72b29cc30a599bd9f6ea61c3e2d22d77c19c40ad
Giulia Pontiggia <giulia95.pontiggia@gmail.com>
2020-08-04 15:19:46 +0200

    Added comments

--- src/main/java/m2/s19ex/Main.java
+++ src/main/java/m2/s19ex/Main.java
@@ -3,8 +3,8 @@ package m2.s19ex;
 public class Main {
     public static void main(String[] args) {
         Person[] persons = new Person[3];
-        persons[0] = new Consultant("Tom", "Jones", 1000);
-        persons[1] = new Employee("Jim", "Smith", 3000);
+        persons[0] = new Consultant("Tom", "Jones", 1000); //1000 = tariffa quotidiana
+        persons[1] = new Employee("Jim", "Smith", 3000); //3000 = salario
         persons[2] = new Consultant("Bob", "Korn", 800);
 
         for (Person person : persons) {
