7af687ed803cb5f1c00068428c4047b0ce84e7be
Giulia Pontiggia <giulia95.pontiggia@gmail.com>
2020-08-04 15:14:34 +0200

    Added comments

--- src/main/java/m2/s19ex/Consultant.java
+++ src/main/java/m2/s19ex/Consultant.java
@@ -1,5 +1,6 @@
 package m2.s19ex;
 
+//Classe derivata da Person
 public class Consultant extends Person {
     private int daily;
 
@@ -8,12 +9,14 @@ public class Consultant extends Person {
         this.daily = daily;
     }
 
+    //Getter -> permette di accedere a dati privati
     public int getDaily() {
         return daily;
     }
-    
+
     @Override
+    //Questo metodo può essere generato in automatico da Ecplipse
     public String toString() {
         return "Consultant " + super.toString() + ": daily bill is " + daily;
-    }
+    } //super.toString() è l'unico modo per vedere firstName e lastName di un Consultant
 }
--- src/main/java/m2/s19ex/Person.java
+++ src/main/java/m2/s19ex/Person.java
@@ -1,5 +1,6 @@
 package m2.s19ex;
 
+//La classe è astratta perché non si vogliono avere oggetti Person
 public abstract class Person {
     private String firstName;
     private String lastName;
