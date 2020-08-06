0250c7a32bba1c072e6f4952974ea4fba3008a3a
Giulia Pontiggia <giulia95.pontiggia@gmail.com>
2020-08-06 13:00:27 +0200

    added comments

--- src/main/java/m4/s07/AsList.java
+++ src/main/java/m4/s07/AsList.java
@@ -10,14 +10,17 @@ public class AsList {
         System.out.println(list);
 
         System.out.println("At index 2: " + list.get(2));
+        //Per aggiugere questo valore gli elementi vengono spostati a destra di un posizione
         list.add(2, -997);
         System.out.println("Index of -997: " + list.indexOf(-997));
 
+        //Elimina l'elemento in posizione 3
         list.remove(3);
         list.set(3, 42);
         System.out.println(list);
     }
 
+    //Creazione di una collezione
     private static List<Integer> aList() {
         return new ArrayList<Integer>(Arrays.asList(12, 18, -5, -2233));
     }
